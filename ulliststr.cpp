9dfc26c77cd2398f2f0c50b58a5351e14445a79f
Sarah Chen <scart11235@gmail.com>
2022-09-12 22:24:37 -0700

    Problem 2 done except valgrind

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,113 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val){
+    //if list does not exist
+    if(tail_ == NULL){
+        Item* item = new Item();
+        item->val[0] = val;
+        item->first = 0;
+        item->last = 1;
+        //set tail to new list
+        tail_ = item;
+        head_ = item;
+    }
+    //if list is full
+    else if(tail_->last >= ARRSIZE){
+        //make a new item
+        Item* item = new Item();
+        item->val[0] = val;
+        item->first = 0;
+        item->last = 1;
+        //set tail to new list
+        item->prev = tail_;
+        tail_->next = item;
+        tail_ = item;
+    }
+    //if list is not full
+    else{
+        tail_->val[tail_->last] = val;
+        tail_->last += 1;
+    }
+    //increment size
+    size_++;
+}
+
+void ULListStr::pop_back(){
+    //if last value is only one in tail_ item
+    if(tail_->last == 1){
+        //Item* temp = tail_;
+        tail_ = tail_->prev;
+        tail_->last = ARRSIZE;
+        //delete[] temp;
+    }
+    //if last value is not only one in tail_ item
+    else{
+        //tail_->val[(int)(tail_->last)-1] = NULL;
+        tail_->last -= 1;
+    }
+    size_--;
+}
+
+void ULListStr::push_front(const std::string& val){
+    //if list doesn't exist yet
+    if(head_ == NULL){
+        Item* item = new Item();
+        item->val[ARRSIZE-1] = val;
+        item->first = ARRSIZE-1;
+        item->last = ARRSIZE;
+        //set tail to new list
+        tail_ = item;
+        head_ = item;
+    }
+    //if list is full
+    else if(head_->first == 0){
+        //make a new item
+        Item* item = new Item();
+        item->val[ARRSIZE-1] = val;
+        item->first = ARRSIZE-1;
+        item->last = ARRSIZE;
+        //set head to new list
+        item->next = head_;
+        head_->prev = item;
+        head_ = item;
+    }
+    //if list is not full
+    else{
+        head_->val[head_->first-1] = val;
+        head_->first -= 1;
+    }
+    size_++;
+}
+
+void ULListStr::pop_front(){
+    //if first is only value left in head_
+    if(head_->first == ARRSIZE-1){
+        //Item* temp = head_;
+        head_ = head_->next;
+        head_->first = 0;
+        //delete[] temp;
+    }
+    //if first value is not only one in head_
+    else{
+        head_->first += 1;
+    }
+    size_--;
+}
+
+std::string const & ULListStr::back() const{
+    if (tail_ == NULL){
+        return NULL;
+    }
+    else {return tail_->val[tail_->last - 1];}
+}
+
+std::string const & ULListStr::front() const{
+    if(head_ == NULL){
+        return NULL;
+    }
+    else {return head_->val[head_->first];}
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -62,4 +169,20 @@ void ULListStr::clear()
   }
   tail_ = NULL;
   size_ = 0;
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+    //see which item in linkedlist index is
+    int itemNum = loc/10;
+    loc %= 10;
+    Item* item = head_;
+    for(int i=0; i<itemNum; i++){
+        item = item->next;
+    }
+    if(&(item->val[loc]) != NULL){
+        return &(item->val[loc]);
+    }
+    else{
+        return NULL;
+    }
 }
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,61 @@
 /* Write your test code for the ULListStr in this file */
 
+#include "ulliststr.h"
 #include <iostream>
 using namespace std;
 
-
+void printList(ULListStr* list, int start, int end);
 int main(int argc, char* argv[])
 {
+    //make a linked list of increasing values
+    ULListStr* list = new ULListStr();
+    list->push_back("three");
+    list->push_back("four");
+    list->push_front("two");
+    //mistake to be fixed (one not seven)
+    list->push_front("seven");
+    //mistake to be fixed (pop this)
+    list->push_front("eight");
+    list->push_back("five");
+    list->push_back("six");
+    list->push_back("seven");
+    list->push_back("eight");
+    list->push_back("nine");
+    list->push_back("ten");
+    list->push_back("eleven");
+    list->push_back("twelve");
+    list->push_back("thirteen");
+    
+    printList(list, 0, 22);
+    
+    //fix mistakes from earlier
+    list->pop_front();
+    list->set((size_t) 8, "one");
+    
+    printList(list, 0, 22);
+    
+    //pop front and back until two items are gone
+    list->pop_front();
+    list->pop_front();
+    list->pop_back();
+    
+    printList(list, 0, 10);
+    
+    //clear
+    list->clear();
+    cout << "size: " << list->size() << endl;
+    return 0;
+}
 
-  return 0;
+void printList(ULListStr* list, int start, int end){
+    cout << "size: " << list->size() << endl;
+    cout << "front: " << list->front() << endl;
+    cout << "back: " << list->back() << endl;
+    if(end > 0){
+        cout<< "whole list: ";
+        for(int i=start; i<end; i++){
+            cout << list->get(i) << ", ";
+        }
+        cout << endl;
+    }
 }
